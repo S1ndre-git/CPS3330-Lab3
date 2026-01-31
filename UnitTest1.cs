d86ed4456cb04fa95992d252da68d14107077e4d
S1ndre-git <tangyuan190517@gmail.com>
2026-01-31 14:10:03 -0500

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using MyKeanLib;
+
+namespace MyKeanLibtest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void MyPow_2Numbers_2Pow3_ShouldBe8()
+    {
+        // arrange
+        var lib = new MyKeanLibrary();
+        double a = 2, b = 3;
+        double expected = 8;
+
+        // act
+        double actual = lib.MyPow(a, b);
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void MyPow_3Numbers_2Pow3Pow4_ShouldBe4096()
+    {
+        // arrange
+        var lib = new MyKeanLibrary();
+        double a = 2, b = 3, c = 4;
+        double expected = 4096;
+
+        // act
+        double actual = lib.MyPow(a, b, c);
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+}
